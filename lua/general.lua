df73b86588bfa82b06c7ed3f1fe7317ad31b0148
Gengchen <tuogengchen@gmail.com>
2022-10-02 17:48:45 -0400

    remove some useless settings and improve formatting

--- lua/general.lua
+++ lua/general.lua
@@ -17,82 +17,67 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
 vim.g.loaded_netrwFileHandlers = 1--}}}
 
-vim.g.mapleader=' '
-vim.g.maplocalleader=','
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ','
 
-vim.opt.secure=true
+vim.o.secure = true
 
--- General settins{{{
-vim.opt.report=0                 -- Don't report on line changes
-vim.opt.visualbell=true               -- Use visual bell instead of beeping
-vim.opt.fileformats={'unix','dos','mac'} -- Use Unix as the standard file type
-vim.opt.fileencodings={'ucs-bom','utf-8','default','latin1','chinese'}
--- vim.opt.path+=/home/thallium/github/acm-algorithm-template/**,**                 " Directories to search when using gf and friends
-vim.opt.virtualedit='block'        -- Position cursor anywhere in visual block
-vim.opt.synmaxcol=2500           -- Don't syntax highlight long lines
-vim.opt.autochdir=true
-vim.opt.updatetime=100
+-- General settings{{{
+vim.o.visualbell = true               -- Use visual bell instead of beeping
+vim.opt.fileencodings:append('chinese')
+vim.o.virtualedit = 'block'        -- Position cursor anywhere in visual block
+vim.o.synmaxcol = 2500           -- Don't syntax highlight long lines
+vim.o.autochdir = true
+vim.o.updatetime = 100
 vim.opt.shortmess:append('c')
-vim.opt.cmdheight=2
-vim.opt.pumheight=12
-vim.opt.cursorline=true
-vim.opt.formatoptions:append('m')        -- fold line with chinese characters
-vim.opt.completeopt={'menuone','noinsert','noselect'}
-vim.opt.foldmethod='marker'
-vim.opt.mouse='a'
+vim.o.cursorline = true
+vim.opt.formatoptions:append('m')        -- break a line at multibyte character, useful for Chinese
+vim.opt.completeopt = { 'menuone', 'noinsert', 'noselect' }
+vim.o.foldmethod = 'marker'
+vim.o.mouse = 'a'
 
-vim.opt.writebackup=false
-vim.opt.undofile=true
-vim.opt.swapfile=false
+vim.o.writebackup = false
+vim.o.undofile = true
+vim.o.swapfile = false
 -- }}}
 -- Tabs and Indents {{{
-vim.opt.expandtab=true       -- expand tabs to spaces
-vim.opt.cindent=true
-vim.opt.tabstop=4       -- The number of spaces a tab is
-vim.opt.shiftwidth=4    -- Number of spaces to use in auto(indent)
-vim.opt.softtabstop=-1  -- Automatically keeps in sync with shiftwidth
-vim.opt.smartindent=true     -- Smart autoindenting on new lines
-vim.opt.shiftround=true      -- Round indent to multiple of 'shiftwidth'
+vim.o.expandtab = true       -- expand tabs to spaces
+vim.o.cindent = true
+vim.o.tabstop = 4       -- The number of spaces a tab is
+vim.o.shiftwidth = 4    -- Number of spaces to use in auto(indent)
+vim.o.softtabstop = -1  -- Automatically keeps in sync with shiftwidth
+vim.o.smartindent = true     -- Smart autoindenting on new lines
+vim.o.shiftround = true      -- Round indent to multiple of 'shiftwidth'
 -- }}}
 -- Searching & replace {{{
-vim.opt.ignorecase=true    -- Search ignoring case
-vim.opt.smartcase=true     -- Keep case when searching with *
-vim.opt.infercase=true     -- Adjust case in insert completion mode
+vim.o.ignorecase = true    -- Search ignoring case
+vim.o.smartcase = true     -- Keep case when searching with *
+vim.o.infercase = true     -- Adjust case in insert completion mode
 
-vim.opt.complete={'.','w','b','k'}  -- C-n completion: Scan buffers, windows and dictionary
-if vim.fn.executable('rg') then
-	vim.opt.grepformat='%f:%l:%m'
-	vim.opt.grepprg='rg --vimgrep'
-elseif vim.fn.executable('ag') then
-	vim.opt.grepformat='%f:%l:%m'
-	vim.opt.grepprg='ag --vimgrep'
-end
+vim.opt.complete = { '.', 'w', 'b', 'k' }  -- C-n completion: Scan buffers, windows and dictionary
 -- }}}
 
 -- Behavior {{{
-vim.opt.linebreak=true                   -- Break long lines at 'breakat'
-vim.opt.breakat=' 	;:,!?'           -- Long lines break chars
+vim.o.linebreak = true                   -- Break long lines at 'breakat'
+vim.o.breakat = ' 	;:,!?'           -- Long lines break chars
 vim.opt.whichwrap:append('<,>,[,],~')    -- Move to following line on certain keys
-vim.opt.splitbelow=true
-vim.opt.splitright=true       -- Splits open bottom right
+vim.o.splitbelow = true
+vim.o.splitright = true       -- Splits open bottom right
 -- }}}
 -- Editor UI {{{
 if vim.fn.has("termguicolors") then
-    vim.opt.termguicolors=true       -- Enable true color
+    vim.opt.termguicolors = true       -- Enable true color
 end
-vim.o.number=true             -- Show number
-vim.opt.showmode=false         -- Don't show mode on bottom
-vim.opt.scrolloff=5         -- Keep at least 2 lines above/below
-vim.opt.fcs='eob: '           -- hide ~ tila
-vim.opt.list=true
-vim.opt.listchars={tab='┆ ',nbsp='+',trail='·',extends='→',precedes='←',eol=''}
-vim.opt.title=true
-vim.opt.titlestring='%t'
-vim.opt.titlelen=95 -- Title length.
-vim.opt.guifont='FiraCode Nerd Font Mono:h13'
+vim.o.number = true             -- Show number
+vim.o.showmode = false         -- Don't show mode on bottom
+vim.o.scrolloff = 5         -- Keep at least 2 lines above/below
+vim.o.fcs = 'eob: '           -- hide ~ tilde
+vim.o.list = true
+vim.opt.listchars = {tab='┆ ',nbsp='+',trail='·',extends='→',precedes='←',eol=''}
+vim.o.title = true
+vim.o.titlestring = '%t'
+vim.o.titlelen = 95 -- Title length.
 
-vim.opt.showmatch=true           -- Jump to matching bracket
-vim.opt.matchtime=1         -- Tenths of a second to show the matching paren
-vim.opt.signcolumn='yes:1'
-vim.o.laststatus=3      -- only show one staus line
+vim.o.laststatus = 3      -- only show one status line
+vim.o.pumheight  =  12 -- max popup menu height
 -- }}}
